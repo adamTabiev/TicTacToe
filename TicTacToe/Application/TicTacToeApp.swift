73a6634f10817fb9ff705087c7bab7c4c0e85d6c
Adam <tabievadam@gmail.com>
2024-10-16 13:46:34 +0200

    11

---
+++ TicTacToe/Application/TicTacToeApp.swift
@@ -0,0 +1,33 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Адам Табиев on 28.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    
+    /// Инициализируем класс роутера по экранам AppRouter
+    @State private var appRouter = AppRouter()
+    /// Инициализируем игровой движок в виде класса GameViewModel
+    @State private var gameVM = GameViewModel()
+    /// Инициализируем менеджер смены языка приложения
+    @State private var languageManager = LanguageManager()
+    
+    var body: some Scene {
+        WindowGroup {
+            Group {
+                OnboardingView()
+            }
+            .environment(appRouter) /// Роутинг по экранам приложения
+            .environment(gameVM) /// Управление состоянием игры, игровой движок
+            .environment(languageManager) /// Управление языком приложения
+            .environment(\.locale, .init(identifier: languageManager.currentLanguage))
+            .preferredColorScheme(.light) /// Приложение всегда работает в светлой теме
+            .dynamicTypeSize(.large) /// Устанавливаем динамический размер текста принудительно
+        }
+    }
+}
